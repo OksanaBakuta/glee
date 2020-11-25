83e937c78750d34c7557730f25ad1038dfd6fda9
OksanaBakuta <oksanabakuta@gmail.com>
2020-11-25 23:15:20 +0200

    changed desing , added video

--- app/js/main.js
+++ app/js/main.js
@@ -8,7 +8,16 @@ $(function(){
     autoplaySpeed: 3000
   });
 
-  var mixer = mixitup('.product-filter__items');
-  var mixer = mixitup('.desing__items');
-
+  var mixer = mixitup('.product-filter__items',{
+    selectors: {
+      control: '.product-filter__btn'
+    }
+  });
+  
+  var mixer = mixitup('.desing__items', {
+    selectors: {
+      control: '.desind-filter__btn'
+    }
+  });
+  
 });
