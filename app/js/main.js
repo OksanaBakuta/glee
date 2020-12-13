8432c0f2e5b0b9e32d4606afce671ba03a5f1fb0
OksanaBakuta <oksanabakuta@gmail.com>
2020-12-13 14:53:41 +0200

    added swiper slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,14 +1,33 @@
 $(function(){
 
-  $('.related-products__slide').slick({
+  var mySwiper = new Swiper('.swiper-container', {
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    keypoard: {
+      enabled: true,
+      onlyInViewport: true,
+      pageUpDown: true,
+    },
+    mousewheel:{
+      sensitivity: 1,
+    },
+    slidesPerView: 4,
+    slidesPerGroup: 4,
+  });
+
+
+
+  /* $('.related-products__slide').slick({
     infinite: false,
     slidesToShow: 4,
     slidesToScroll: 1,
-    /* autoplay: true,
-    autoplaySpeed: 3000, */
+    autoplay: true,
+    autoplaySpeed: 3000,
     prevArrow: '<button class="slick-prev" type="button"><svg xmlns="http://www.w3.org/2000/svg" width="22" height="9" viewBox="0 0 22 9"><path d="M1460.7,1330.53h-16.94l0.1-2.86a0.3,0.3,0,0,0,.08-0.2,0.323,0.323,0,0,0-.08-0.21l-0.18-.17a0.267,0.267,0,0,0-.4,0l-4.2,4.21a0.3,0.3,0,0,0-.08.2,0.323,0.323,0,0,0,.08.21l4.2,4.21a0.29,0.29,0,0,0,.4,0l0.18-.17a0.323,0.323,0,0,0,.08-0.21,0.3,0.3,0,0,0-.08-0.2l-0.12-2.86h16.97a0.3,0.3,0,0,0,.29-0.3v-1.37A0.291,0.291,0,0,0,1460.7,1330.53Z" transform="translate(-1439 -1327)" /></svg ></button>',
     nextArrow: '<button class="slick-next" type="button"><svg xmlns="http://www.w3.org/2000/svg" width="22" height="9" viewBox="0 0 22 9"><path class="cls-1" d="M1509.3,1332.47h16.94l-0.1,2.86a0.3,0.3,0,0,0-.08.2,0.323,0.323,0,0,0,.08.21l0.18,0.17a0.267,0.267,0,0,0,.4,0l4.2-4.21a0.3,0.3,0,0,0,.08-0.2,0.323,0.323,0,0,0-.08-0.21l-4.2-4.21a0.29,0.29,0,0,0-.4,0l-0.18.17a0.323,0.323,0,0,0-.08.21,0.318,0.318,0,0,0,.08.2l0.12,2.86h-16.97a0.3,0.3,0,0,0-.29.3v1.37A0.291,0.291,0,0,0,1509.3,1332.47Z" transform="translate(-1509 -1327)" /></svg ></button>',
-  });
+  }); */
   
   $('.product-tabs__top-item').on('click', function(e){
     e.preventDefault();
